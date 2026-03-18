c87301404abe079013b9d84c06ee19cd9b88549f
William Quayson <wgsquayson@gmail.com>
2026-03-17 23:35:06 -0300

    feat: add seo tags on frontend routes

--- front/src/app/login.tsx
+++ front/src/app/login.tsx
@@ -3,13 +3,26 @@ import Layout from "@/components/layout";
 import Input from "@/components/input";
 import Button from "@/components/button";
 
+import Head from "expo-router/head";
+
 export default function Login() {
   return (
-    <Layout>
-      <Text variant="title">Login to Expo Router SSR</Text>
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
-      <Button text="Login" />
-    </Layout>
+    <>
+      <Head>
+        <title>Login | Expo Router SSR</title>
+        <meta
+          name="description"
+          content="Login to use the Expo Router SSR app."
+        />
+      </Head>
+
+      <Layout>
+        <Text variant="title">Login to Expo Router SSR</Text>
+        <Input placeholder="Email" />
+        <Input placeholder="Password" />
+        <Button text="Login" />
+        <Text variant="bold">Sign up</Text>
+      </Layout>
+    </>
   );
 }
--- front/src/app/signup.tsx
+++ front/src/app/signup.tsx
@@ -3,14 +3,27 @@ import Layout from "@/components/layout";
 import Input from "@/components/input";
 import Button from "@/components/button";
 
+import Head from "expo-router/head";
+
 export default function SignUp() {
   return (
-    <Layout>
-      <Text variant="title">Sign Up to Expo Router SSR</Text>
-      <Input placeholder="Name" />
-      <Input placeholder="Email" />
-      <Input placeholder="Password" />
-      <Button text="Login" />
-    </Layout>
+    <>
+      <Head>
+        <title>Sign up | Expo Router SSR</title>
+        <meta
+          name="description"
+          content="Create an account to use the Expo Router SSR app."
+        />
+      </Head>
+
+      <Layout>
+        <Text variant="title">Sign Up to Expo Router SSR</Text>
+        <Input placeholder="Name" />
+        <Input placeholder="Email" />
+        <Input placeholder="Password" />
+        <Button text="Sign up" />
+        <Text variant="bold">Go back</Text>
+      </Layout>
+    </>
   );
 }
