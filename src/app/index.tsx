1a1f457d7276f3e7c568c693ed77054cff1b419f
William Quayson <wgsquayson@gmail.com>
2026-03-19 12:50:49 -0300

    chore: return null on homescreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,3 @@
-import { getTokensFromCookies } from "@/utils/auth";
-import { Redirect, useLoaderData } from "expo-router";
-import { LoaderFunction } from "expo-server";
-
-type LoaderReturn = { redirect: "/login" | "/dashboard" };
-
-export const loader: LoaderFunction<LoaderReturn> = async (request) => {
-  const { accessToken, refreshToken } = getTokensFromCookies(request);
-
-  if (accessToken && refreshToken) {
-    return { redirect: "/dashboard" };
-  }
-
-  return { redirect: "/login" };
-};
-
 export default function HomeScreen() {
-  const { redirect } = useLoaderData<typeof loader>();
-
-  if (!redirect) return null;
-
-  return <Redirect href={redirect} />;
+  return null;
 }
