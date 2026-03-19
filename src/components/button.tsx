48730787d31f3ca9ea8f68a888bf65277f53c9db
William Quayson <wgsquayson@gmail.com>
2026-03-19 12:30:43 -0300

    chore: add disabled style to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,6 +2,7 @@ import {
   StyleSheet,
   TouchableOpacity,
   TouchableOpacityProps,
+  ViewStyle,
 } from "react-native";
 import Text from "./text";
 
@@ -10,8 +11,13 @@ type ButtonProps = TouchableOpacityProps & {
 };
 
 export default function Button({ text, ...props }: ButtonProps) {
+  const style: ViewStyle = {
+    ...styles.button,
+    opacity: props.disabled ? 0.6 : 1,
+  };
+
   return (
-    <TouchableOpacity {...props} style={styles.button}>
+    <TouchableOpacity {...props} style={style}>
       <Text variant="bold" color="white">
         {text}
       </Text>
